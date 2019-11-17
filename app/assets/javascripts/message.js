81dcabba475d7effcfb9884d55531765919accdb
ebi-jp <kazu3306@icloud.com>
2019-11-17 14:20:18 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,22 +3,21 @@ $(function(){
     var addImage = message.image? `<img class ="lower-message__image", src="${message.image}">`: '' ;
     var html = `<div class="message">
                   <div class="upper-message">
-                  <div class="upper-message__user-name">
-                  ${message.user_name}
-                  </div>
-                  <div class="upper-message__date">
-                  ${message.created_at}
-                  </div>
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.created_at}
+                    </div>
                   </div>
                   <div class="lower-message">
-                  ${addImage}
-                  <p class="lower-message__content">
-                  ${message.content}
-                  </p>
+                    ${addImage}
+                    <p class="lower-message__content">
+                    ${message.content}
+                    </p>
                   </div>
-                  </div>`
-                return html;
-                
+                </div>`
+    return html;
   }
 
   $('#new_message').on('submit', function(e){
