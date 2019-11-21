a489de70a3f3f88213e4e27d7fbbeaa24e8f8aea
ebi-jp <57351121+ebi-jp@users.noreply.github.com>
2019-11-21 14:38:24 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,8 @@
 $(function(){
+
   function buildHTML(message){
     var addImage = message.image? `<img class ="lower-message__image", src="${message.image}">`: '' ;
-    var html = `<div class="message">
+    var html = `<div class="message" data-message_id="${message.id}">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
                       ${message.user_name}
@@ -50,7 +51,98 @@ $(function(){
       alert("メッセージ送信に失敗しました");
   });
   })
-})
+
+  // var buildMessageHTML = function(message) {
+  //   if (message.content && message.image.url) {
+  //     //data-idが反映されるようにしている
+  //     var html = `<div class="message" data-id= "${message.id}>
+  //                   <div class="upper-message">
+  //                     <div class="upper-message__user-name">
+  //                       ${message.user_name}
+  //                     </div>
+  //                     <div class="upper-message__date">
+  //                       ${message.created_at}
+  //                     </div>
+  //                   </div>
+  //                   <div class="lower-message">
+  //                     <p class="lower-message__content">
+  //                       ${message.content}
+  //                     </p>
+  //                     <img src="${message.image.url}" class="lower-message__image" >
+  //                   </div>
+  //                 </div>`
+  //   } else if (message.content) {
+  //     //同様に、data-idが反映されるようにしている
+  //     var html = `<div class="message" data-id=${message.id}>
+  //                 <div class="upper-message">
+  //                   <div class="upper-message__user-name">
+  //                     ${message.user_name}
+  //                   </div>
+  //                   <div class="upper-message__date">
+  //                     ${message.created_at}
+  //                   </div>
+  //                 </div>
+  //                 <div class="lower-message">
+  //                   <p class="lower-message__content">
+  //                     ${message.content}
+  //                   </p>
+  //                   </div>
+  //               </div>`
+  //   } else if (message.image.url) {
+  //     //同様に、data-idが反映されるようにしている
+  //     var html = `<div class="message" data-id=${message.id} >
+  //                 <div class="upper-message">
+  //                   <div class="upper-message__user-name">
+  //                     ${message.user_name}
+  //                   </div>
+  //                   <div class="upper-message__date">
+  //                     ${message.created_at}
+  //                   </div>
+  //                 </div>
+  //                 <div class="lower-message">
+  //                   <img src="${message.image.url}" class="lower-message__image" >
+  //                 </div>
+  //               </div>`
+  //   };
+  //   return html;
+  // };
+
+    var reloadMessages = function() {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+       var last_message_id = $('.message').last().data('message_id');
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: "api/messages",
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'GET',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      
+      })
+      .done(function(messages) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        // console.log(messages)
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        messages.forEach(function (message) {
+          // メッセージが入ったHTMLを取得
+          
+          // console.log(message);
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+        //メッセージを追加
+        $('.messages').animate({scrollTop : $('.messages')[0].scrollHeight},'fast');
+        })
+      })
+      .fail(function() {
+          alert("失敗しました")
+    });
+  }
+  }
+  setInterval(reloadMessages, 5000);
+});
 
 
 
