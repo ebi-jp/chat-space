ddb8c86238cf3749e944c83f4b074d17bec88e64
ebi-jp <57351121+ebi-jp@users.noreply.github.com>
2019-11-21 15:31:42 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -52,61 +52,7 @@ $(function(){
   });
   })
 
-  // var buildMessageHTML = function(message) {
-  //   if (message.content && message.image.url) {
-  //     //data-idが反映されるようにしている
-  //     var html = `<div class="message" data-id= "${message.id}>
-  //                   <div class="upper-message">
-  //                     <div class="upper-message__user-name">
-  //                       ${message.user_name}
-  //                     </div>
-  //                     <div class="upper-message__date">
-  //                       ${message.created_at}
-  //                     </div>
-  //                   </div>
-  //                   <div class="lower-message">
-  //                     <p class="lower-message__content">
-  //                       ${message.content}
-  //                     </p>
-  //                     <img src="${message.image.url}" class="lower-message__image" >
-  //                   </div>
-  //                 </div>`
-  //   } else if (message.content) {
-  //     //同様に、data-idが反映されるようにしている
-  //     var html = `<div class="message" data-id=${message.id}>
-  //                 <div class="upper-message">
-  //                   <div class="upper-message__user-name">
-  //                     ${message.user_name}
-  //                   </div>
-  //                   <div class="upper-message__date">
-  //                     ${message.created_at}
-  //                   </div>
-  //                 </div>
-  //                 <div class="lower-message">
-  //                   <p class="lower-message__content">
-  //                     ${message.content}
-  //                   </p>
-  //                   </div>
-  //               </div>`
-  //   } else if (message.image.url) {
-  //     //同様に、data-idが反映されるようにしている
-  //     var html = `<div class="message" data-id=${message.id} >
-  //                 <div class="upper-message">
-  //                   <div class="upper-message__user-name">
-  //                     ${message.user_name}
-  //                   </div>
-  //                   <div class="upper-message__date">
-  //                     ${message.created_at}
-  //                   </div>
-  //                 </div>
-  //                 <div class="lower-message">
-  //                   <img src="${message.image.url}" class="lower-message__image" >
-  //                 </div>
-  //               </div>`
-  //   };
-  //   return html;
-  // };
-
+  
     var reloadMessages = function() {
       if (window.location.href.match(/\/groups\/\d+\/messages/)){
       //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
@@ -119,17 +65,13 @@ $(function(){
         dataType: 'json',
         //dataオプションでリクエストに値を含める
         data: {id: last_message_id}
-      
       })
       .done(function(messages) {
         //追加するHTMLの入れ物を作る
         var insertHTML = '';
-        // console.log(messages)
         //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         messages.forEach(function (message) {
           // メッセージが入ったHTMLを取得
-          
-          // console.log(message);
           insertHTML = buildHTML(message);
           $('.messages').append(insertHTML);
         //メッセージを追加
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,13 +1,9 @@
 class Api::MessagesController < ApplicationController
   def index
     group = Group.find(params[:group_id])
-    # @group = Group.find(params[:group_id])
+
     last_message_id = params[:id].to_i
     @messages = group.messages.includes(:user).where("id > #{last_message_id}")
-    # @messages = group.messages.includes(:user).where('id > ?' ,params[:last_id])
-    # respond_to do |format|
-    #   format.html
-    #   format.json
-    # end
+
   end
 end 
